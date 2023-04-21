6b5da1c5069409df0f514c7ec6db738e7cfa2426
JulienB <julien.barbe@agregio-edf.com>
2023-04-21 15:41:02 +0200

    Init project

---
+++ src/main/java/com/example/playgroundopenidconnect/DemoOpenIdConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.playgroundopenidconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenIdConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenIdConnectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/playgroundopenidconnect/controller/Message.java
@@ -0,0 +1,4 @@
+package com.example.playgroundopenidconnect.controller;// src/main/java/com/auth0/example/model/Message.java
+
+public record Message(String message) {
+}
